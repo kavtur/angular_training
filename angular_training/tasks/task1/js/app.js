dfc2fa292f64c25fd5b98e014713d9e42273cd2b
dmitrykavtur <dmitrykavtur@coherentsolutions.com>
2015-10-27 21:50:35 +0300

    implemented task1

---
+++ angular_training/tasks/task1/js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* App Module */
+var testApp = angular.module('testApp', []);
+
+testApp.controller('ParentCtrl1', ['$scope', function($scope){
+	
+}]);
+
+
+testApp.controller('ParentCtrl2', ['$scope', function($scope){
+	
+}]);
+
+testApp.controller('ChildCtrl1', ['$scope', function($scope){
+	
+}]);
+
+testApp.controller('ChildCtrl2', ['$scope', function($scope){
+	
+}]);
