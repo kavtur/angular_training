83b86b9e6f2f4f5fb42d7c55eb6f684863f1b8fd
Vladislav Kondratenko <VladislavKondratenko@coherentsolutions.com>
2015-11-02 15:45:52 +0300

    commit

--- app/task4/js/custom.js
+++ app/task4/js/custom.js
@@ -1,6 +1,4 @@
-
 module=angular.module('task4', [])
-
 function changeElementPosition(scope, item, allItems){
         var newItems=[]
         for(var rootItem in allItems){
@@ -10,25 +8,21 @@ function changeElementPosition(scope, item, allItems){
         }
       return newItems;  
 }
-
 module.directive('basket', function() {
     return {
       restrict: 'E',
       controller: ['$scope', function($scope) {
         var basketItems = $scope.basketItems = [];
-
       $scope.$on('ADD_TO_BASKET', function(event, item) {
         $scope.basketItems.push(item);
         event.targetScope.items=changeElementPosition($scope,item,event.targetScope.items);
       });
-
       $scope.removeFromBasket = function(item) {
             $scope.$broadcast('REMOVE_FROM_BASKET', item);           
         };
       }]
     };
   });
-
 module.directive('itemsList', function() {
     return {
       require: '^basket',
@@ -38,7 +32,6 @@ module.directive('itemsList', function() {
         $scope.addToBasket = function(item) {
             $scope.$broadcast('ADD_TO_BASKET', item);           
         };
-
       $scope.$on('REMOVE_FROM_BASKET', function(event, item) {
         $scope.items.push(item);
         event.targetScope.basketItems=changeElementPosition($scope,item,event.targetScope.basketItems);
