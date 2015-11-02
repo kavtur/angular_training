db8e3a00f2bef906f01bd5e13fe7e2cb9fe639f6
Vladislav Kondratenko <VladislavKondratenko@coherentsolutions.com>
2015-11-02 09:12:57 +0300

    task1 merged

---
+++ app/task1/js/custom.js
@@ -0,0 +1,16 @@
+var appModule = angular.module('task1', []);
+appModule.controller('parentCtrl1', function ($scope) {
+  
+});
+
+appModule.controller('parentCtrl2',  function ($scope) {
+  
+});
+
+appModule.controller('ChildCtrl1', function ($scope) {
+  
+});
+
+appModule.controller('ChildCtrl2',  function ($scope) {
+  
+});
