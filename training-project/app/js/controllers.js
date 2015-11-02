c99845beffdcc2281aee63ea62018b21ebb382fc
Alexey Marchenko <alexeymarchenko@cohorentsolutions.com>
2015-11-02 23:11:42 +0300

    task 1

---
+++ training-project/app/js/controllers.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* Controllers */
+
+var trainingApp = angular.module('trainingApp', []);
+
+trainingApp.controller('parentCtrl1', function($scope) {
+});
+
+trainingApp.controller('parentCtrl2', function($scope) {
+});
+
+trainingApp.controller('ChildCtrl1', function($scope) {
+});
+
+trainingApp.controller('ChildCtrl2', function($scope) {
+});
